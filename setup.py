fadb62dd586217dbe3e2498fa8d8c45a5cb4ea5a
Michele <michelemilidoni@gmail.com>
2014-05-10 00:27:38 +0200

    setup fixed

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@
 
 from pip.req import parse_requirements
 from setuptools import setup
-from subprocess import call
 
 install_reqs = parse_requirements("requirements.txt")
 reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name="Rubiz",
     version="0.1-alpha",
+    description="Simple, versatile address book",
+    author="Michele Milidoni",
+    author_email="michelemilidoni@gmail.com",
+    url="https://github.com/mmilidoni/rubiz",
     install_requires=reqs
 )
 
-call("pip install -e git://github.com/benjiec/django-tables2-simplefilter.git#egg=django_tables2_simplefilter", shell=True)
